af4ee4ec353834e2888e63077576d4dd1894f100
Gil Jeremy Ditablan <71557609+destinyarx@users.noreply.github.com>
2025-03-28 00:18:18 +0800

    set inital page component

---
+++ src/components/Content.tsx
@@ -0,0 +1,11 @@
+const Content = () => {
+    return (
+        <div className="h-[80vh] border-4 border-red-500">
+            <div className="text-xl">
+                This is the main Content
+            </div>
+        </div>
+    )
+}
+
+export default Content;
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,9 @@
+const Navbar = () => {
+    return (
+        <>
+            <div className="text-3xl"> My Initial Portfolio </div>
+        </>
+    )
+}
+
+export default Navbar;

88a937f704f5587c92ea7c9902ac124a1767dc71
Gil Jeremy Ditablan <71557609+destinyarx@users.noreply.github.com>
2025-04-03 03:48:15 +0800

    Update WorkExperience.tsx

--- src/components/WorkExperience.tsx
+++ src/components/WorkExperience.tsx
@@ -1,5 +1,23 @@
 import WorkExperienceCard from './ui/WorkExperienceCard';
 
+const experience = [
+    { 
+        title: "Full Stack Developer",
+        company: "NCMH",
+        dateRange: "Aug. 2022 – Present" 
+    },
+    { 
+        title: "Web Developer (Internship)",
+        company: "Avasia Information Systems, Inc.",
+        dateRange: "Aug. 2021 – Sep. 2021" 
+    },
+    { 
+        title: "Full Stack Developer",
+        company: "TESDA Rizal Provincial Office (Special Program for Employment of Student)",
+        dateRange: " April 2018 – May 2018" 
+    },
+]
+
 const WorkExperience = () => {
     return (
         <div className="flex flex-col gap-5 min-h-[20vh] mt-20 mb-10">
@@ -7,9 +25,9 @@ const WorkExperience = () => {
                 Work Experience
             </div>
 
-            <WorkExperienceCard title = "Full Stack Developer" company="NCMH" dateRange="Aug. 2022 – Present"/>
-            <WorkExperienceCard title = "Web Developer (Internship)" company="Avasia" dateRange="Aug. 2021 – Sep. 2021"/>
-            <WorkExperienceCard title = "Data Entry" company="TESDA Rizal Provincial Office (Special Program for Employment of Student)" dateRange="April 2018 – May 2018"/>
+            {experience.map((data, index) => (
+                <WorkExperienceCard key={index} title={data.title} company={data.company} dateRange={data.dateRange}/>
+            ))}
         </div>
     )
 }

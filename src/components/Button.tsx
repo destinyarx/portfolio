a3736c5e1630d8517055a5283b30235de7d593b8
Gil Jeremy Ditablan <71557609+destinyarx@users.noreply.github.com>
2025-04-02 10:37:51 +0800

    Update Button.tsx

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -2,7 +2,7 @@ const Button = ({ text, color=null, icon=null }: { text: string, color?: string|
     return (
         <div className={`inline-block items-center w-auto border-1 border-amber-50 text-white text-sm rounded-full py-2 px-4 ${color || ''}`}>
             { text }
-            {/* {  icon && <span> { icon } </span> } */}
+            {  icon && <span> </span> }
 
         </div>
     )

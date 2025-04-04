536aa2074a54ea3226c6318d08d30c2ee630bf12
Gil Jeremy Ditablan <71557609+destinyarx@users.noreply.github.com>
2025-04-04 09:50:43 +0800

    skills section - 80% progress

--- src/components/ui/SkillsBadge.tsx
+++ src/components/ui/SkillsBadge.tsx
@@ -1,12 +1,19 @@
-const SkillsBadge = ({title, children}: {title:string, children?: React.ReactNode}) => {
+const SkillsBadge = ({title, size = null, children}: {title:string, size?: string|null, children?: React.ReactNode}) => {
+    const [first, second] = title.split(' ');
+
     return (
         <div className="rounded-full">
           <div className="inline-block border-2 border-slate-400 bg-slate-800 rounded-lg p-1">
             {children}
-            
-            <div className="text-xs text-gray-300">
-                {title}
+            <div className={`text-gray-300 ${second ? '-mt-1' : 'mt-1'} ${size ?? 'text-xs'}`}>
+                {second ? (
+                    <span>
+                        {first}<br/>
+                        <span className="mt-[-0.25rem]">{second}</span>
+                    </span>
+                ) : title}
             </div>
+
           </div>
         </div>
     );

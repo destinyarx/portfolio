bbdb90bce20881008cf6380759502d6423ee9d0a
Gil Jeremy Ditablan <71557609+destinyarx@users.noreply.github.com>
2025-04-06 05:12:12 +0800

    modify nav bar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,18 +1,44 @@
+import { useState } from 'react';
+
 const Navbar = () => {
+    const [selectedSection, setSelectedSection] = useState<string>('About');
+    
+    const sections = [
+        { title: 'About', path: '/' },
+        { title: 'Experience', path: '/' },
+        { title: 'Skills', path: '/' },
+        { title: 'Projects', path: '/' },
+    ];
+
     return (
-        <div>
-            <nav className="w-full bg-gray-900 text-white py-4">
+        <div className="relative">
+            <nav className="w-full bg-gradient-to-r from-stone-950 via-neutral-800 to-stone-900 text-white py-4">
                 <div className="flex justify-between px-1">
                     <div className="text-lg font-bold">My Portfolio</div>
 
-                    <div className="space-x-4">
-                        <a href="#skills" className="hover:text-gray-400">About</a>
-                        <a href="#work-experience" className="hover:text-gray-400">Work Experience</a>
-                        <a href="#skills" className="hover:text-gray-400">Skills</a>
-                        <a href="#skills" className="hover:text-gray-400">Projects</a>
+                    <div className="space-x-6 text-lg font-extrabold">
+                        {sections.map((section, index) => (
+                            <a 
+                                key={index} 
+                                href={section.path} 
+                                className={`hover:text-gray-400 transition-all duration-300 ${
+                                    selectedSection === section.title 
+                                    ? 'text-2xl' 
+                                    : 'text-lg'
+                                }`}
+                                onClick={(e) => {
+                                    e.preventDefault();
+                                    setSelectedSection(section.title);
+                                }}
+                            >
+                                {section.title}
+                            </a>
+                        ))}
                     </div>
                 </div>
             </nav>
+
+            <div className="absolute left-0 right-0 h-[1px] bg-gradient-to-r from-transparent via-gray-300 to-transparent top-full" />
         </div>
     )
 }
